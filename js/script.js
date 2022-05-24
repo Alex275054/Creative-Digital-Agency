a6fd16cceffbbacd52c66bbdaabb70f1796fc42f
Alex275054 <55253968+Alex275054@users.noreply.github.com>
2022-05-24 15:32:31 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,7 +16,32 @@ btn.on('click', function (e) {
 
 //button-up
 
-// slider
+//button-up2
+$(function() {
+	// при нажатии на кнопку scrollup
+	$('.scrollup').click(function() {
+	  // переместиться в верхнюю часть страницы
+	$("html, body").animate({
+		scrollTop:0
+	},1000);
+	})
+})
+	// при прокрутке окна (window)
+	$(window).scroll(function() {
+	// если пользователь прокрутил страницу более чем на 200px
+	if ($(this).scrollTop()>200) {
+		// то сделать кнопку scrollup видимой
+		$('.scrollup').fadeIn();
+	}
+	// иначе скрыть кнопку scrollup
+	else {
+		$('.scrollup').fadeOut();
+	}
+	});
+ //button-up2
+
+
+//slider
 $(document).ready(function () {
 	$('.second__items').slick({
 		arrows: true,
